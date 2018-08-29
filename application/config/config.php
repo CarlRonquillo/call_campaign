ee00f5a24d547585de985164357365d92be4b5d2
cronquillo <cronquillo@nazarene.org>
2018-08-30 01:43:37 +0800

    production

--- application/config/config.php
+++ application/config/config.php
@@ -23,8 +23,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-//$config['base_url'] = 'http://campaign.ncmmanila.org/';
-$config['base_url'] = 'http://localhost/campaign/';
+$config['base_url'] = 'http://campaign.ncmmanila.org/';
+//$config['base_url'] = 'http://localhost/campaign/';
 /*
 |--------------------------------------------------------------------------
 | Index File
